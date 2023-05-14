7617745c0518c82110d2739e916fe0d2ef208517
BlackSudo <omersee555@gmail.com>
2023-05-14 11:07:31 +0200

    URL & IP reports backend v1.2

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -211,7 +211,7 @@ def cases(case_id):
                 submission.url = urlip if data_type == "url" else None
                 submission.ip = urlip if data_type == "ip" else None
                 submission.analysis_id = result.id if hasattr(result, 'id') else None
-                submission.malicious = result.last_analysis_stats['malicious'] if hasattr(result, 'stats') else result.total_votes['malicious']
+                submission.malicious = result.last_analysis_stats['malicious'] if hasattr(result, 'last_analysis_stats') else result.stats['malicious']
                 submission.scan_status = result.status if hasattr(result, 'status') else "completed"
                 
                 db.session.add(submission)
